94659ecab2603e2d7857df06b1be7ff9565658b5
Camilo Ríos Hernandez <camilorios92@hotmail.com>
2024-06-06 22:56:02 -0500

    Fix, duplicado en div información
    Signed-off-by: Camilo Ríos Hernandez <camilorios92@hotmail.com>

--- application/views/admin/organizaciones/informacion.php
+++ application/views/admin/organizaciones/informacion.php
@@ -6,8 +6,8 @@
  *
  */
 ?>
-<div id="idSolicitudInfo" style="display: none"><?php echo $idSolicitud; ?></div>
-<div id="idOrganizacion" style="display: none"><?php echo $idOrganizacion; ?></div>
+<div id="idSolicitudInfo" style="display: none"><?= $idSolicitud; ?></div>
+<div id="idOrganizacion" style="display: none"><?= $idOrganizacion; ?></div>
 <!-- Registro de observaciones por formulario -->
 <div class="container" id="">
 	<div class="panel-group" id="datos_org_final">
@@ -81,70 +81,11 @@
 				</div>
 			</div>
 		</div>
-			<hr />
-			<div class="col-md-4">
-				<div class="form-group">
-					<p>Nombre de la organización:</p><label class="tipoLeer" id='nOrgSol'></label>
-				</div>
-				<div class="form-group">
-					<p>Sigla:</p><label class="tipoLeer" id='sOrgSol'></label>
-				</div>
-				<div class="form-group">
-					<p>Número NIT:</p><label class="tipoLeer" id='nitOrgSol'></label>
-				</div>
-				<div class="form-group">
-					<p>Nombre del representante:</p><label class="tipoLeer" id='nrOrgSol'></label>
-				</div>
-				<div class="form-group">
-					<p>Correo de la organización:</p><label class="tipoLeer" id='cOrgSol'></label>
-				</div>
-				<div class="clearfix"></div>
-			</div>
-			<div class="col-md-4">
-				<div class="form-group">
-					<p>Fecha de creación de la cuenta:</p><label class="tipoLeer" id='fechaSol'></label>
-				</div>
-				<div class="form-group">
-					<p>ID de la solicitud:</p><label class="tipoLeer" id='idSol'></label>
-				</div>
-				<div class="form-group">
-					<p>Tipo de solicitud:</p><label class="tipoLeer" id='tipoSol'></label>
-				</div>
-				<div class="form-group">
-					<p>Modalidad de la solicitud:</p><label class="tipoLeer" id='modSol'></label>
-				</div>
-				<div class="form-group">
-					<p>Motivo de la solicitud:</p><label class="tipoLeer" id='motSol'></label>
-				</div>
-				<div class="clearfix"></div>
-			</div>
-			<div class="col-md-4">
-				<div class="form-group">
-					<p>Fecha de finalización de la solicitud:</p><label class="tipoLeer" id='revFechaFin'></label>
-				</div>
-				<div class="form-group">
-					<p>Numero de solicitudes:</p><label class="tipoLeer" id='numeroSol'></label>
-				</div>
-				<div class="form-group">
-					<p>Revisión #:</p><label class="tipoLeer" id='revSol'></label>
-				</div>
-				<div class="form-group">
-					<p>Fecha de última revisión:</p><label class="tipoLeer" id='revFechaSol'></label>
-				</div>
-				<div class="form-group">
-					<p>Estado de la organización:</p><label class="tipoLeer" id='estOrg'></label>
-				</div>
-				<div class="form-group">
-					<p>Cámara de comercio: <a href="" id="camaraComercio_org" target="_blank">Clic aquí para ver la cámara de comercio</a></p>
-				</div>
-				<div class="clearfix"></div>
-			</div>
-		</div>
 		<div class="clearfix"></div>
 		<hr />
 		<button class="btn btn-danger btn-sm pull-left" id="admin_ver_finalizadas_volver"><i class="fa fa-arrow-left" aria-hidden="true"></i> Volver al panel principal</button>
 		<button class="btn btn-sm btn-warning pull-right" id="verRelacionCambios" data-toggle='modal' data-target='#modalRelacionCambios'>Ver relacion de cambios <i class="fa fa-eye" aria-hidden="true"></i></button>
-		<button class="btn btn-sm btn-info pull-right" data-toggle='modal' data-target='#modalPedirCamara'>Pedir cámara de comercio <i class="fa fa-refresh" aria-hidden="true"></i></button>
+		<button class="btn btn-sm btn-info pull-right" id='actualizar_solicitud' data-solicitud="<?= $idSolicitud ?>" disabled>Ir a la solicitud de usuario <i class="fa fa-refresh" aria-hidden="true"></i></button>
 		<button class="btn btn-siia btn-sm pull-right verHistObs" id="hist_org_obs" data-backdrop="false" data-toggle='modal' data-target='#verHistObs'>Historial de observaciones <i class="fa fa-history" aria-hidden="true"></i></button>
 		<div class="clearfix"></div>
 		<hr />
