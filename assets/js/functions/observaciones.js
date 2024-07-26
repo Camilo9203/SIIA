5341adebde0e141a7c7e937fed8e505ea551e17e
Camilo Ríos Hernandez <camilorios92@hotmail.com>
2024-07-25 21:30:59 -0500

    Fix camara de comercio inscritas
    Signed-off-by: Camilo Ríos Hernandez <camilorios92@hotmail.com>

--- assets/js/functions/observaciones.js
+++ assets/js/functions/observaciones.js
@@ -644,6 +644,7 @@ if ($("#idSolicitudInfo").html() != undefined) {
 				$("#revSol").html(response.solicitudes.numeroRevisiones);
 				$("#revFechaSol").html(response.solicitudes.fechaUltimaRevision);
 				$("#revFechaUltimaActualizacion").html(response.estadoOrganizaciones.fechaUltimaActualizacion);
+				$("#camaraComercio_org").attr("href", baseURL + "uploads/camaraComercio/" + response.organizaciones.camaraComercio);
 				$("#estOrg").html(response.estadoOrganizaciones.nombre);
 				$("#nOrgSol").html(response.organizaciones.nombreOrganizacion);
 				$("#sOrgSol").html(response.organizaciones.sigla);
